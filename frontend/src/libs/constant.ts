50201ce2a02155603b3a46143bb58d4599d12305
qingxuan.luan <qingxuan.luan@porsche.digital>
2024-06-20 15:42:31 +0800

    feat: delete other tabs

--- frontend/src/libs/constant.ts
+++ frontend/src/libs/constant.ts
@@ -1,23 +1,25 @@
+import pdcLogo from "@/assets/images/pdc_logo.png";
+
 export const TOAST_ICONS_MAP = {
-  success: 'icon-xiugaichenggong',
-  error: 'icon-error',
-  warning: 'icon-tishi',
-  loading: 'icon-loading',
+  success: "icon-xiugaichenggong",
+  error: "icon-error",
+  warning: "icon-tishi",
+  loading: "icon-loading",
 };
 
 export const placeholderImg =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAABdJREFUKFNjZCASMBKpjmFUId6QIjp4AAppAAuXjCs4AAAAAElFTkSuQmCC';
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAABdJREFUKFNjZCASMBKpjmFUId6QIjp4AAppAAuXjCs4AAAAAElFTkSuQmCC";
 
-export const DEFAULT_LANGUAGE = "zh";  
+export const DEFAULT_LANGUAGE = "zh";
 export const DEFAULT_INPUT_FUNCS = [
   { title: "文本问答", type: "text_chat", code: "text_chat" },
-  { title: "文档解析", type: "doc_chat", code: "doc_chat" },
-  { title: "表格问答", type: "text_chat", code: "table_chat" },
+  // { title: "文档解析", type: "doc_chat", code: "doc_chat" },
+  // { title: "表格问答", type: "text_chat", code: "table_chat" },
 ];
 export const DEFAULT_PAGE_CONFIG = {
-  headerLogo: "https://img.alicdn.com/imgextra/i2/O1CN01KmdQ0922NyXlOZFIv_!!6000000007109-2-tps-832-96.png",
+  headerLogo: pdcLogo,
   content: {
-    logo: "https://img.alicdn.com/imgextra/i2/O1CN01tjE7uZ1GGYZTUqq7L_!!6000000000595-2-tps-1248-144.png",
+    logo: pdcLogo,
     guideText: "你好，我是阿里云百炼大模型",
   },
   answer: {
@@ -27,35 +29,34 @@ export const DEFAULT_PAGE_CONFIG = {
     {
       showData: "轻松获得金牌营销文案",
       contentData: "轻松获得金牌营销文案",
-      iconUrl: "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
+      iconUrl:
+        "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
       title: "撰写营销文案",
     },
     {
-      showData:
-        "你也可以高情商",
-      contentData:
-        "你也可以高情商",
-      iconUrl: "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
+      showData: "你也可以高情商",
+      contentData: "你也可以高情商",
+      iconUrl:
+        "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
       title: "高情商回复",
     },
     {
-      showData:
-        "制定专属健身计划",
-      contentData:
-        "制定专属健身计划",
-      iconUrl: "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
+      showData: "制定专属健身计划",
+      contentData: "制定专属健身计划",
+      iconUrl:
+        "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
       title: "AI 健身教练",
     },
     {
-      showData:
-        "送情书一直都很浪漫",
-      contentData:
-        "送情书一直都很浪漫",
-      iconUrl: "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
+      showData: "送情书一直都很浪漫",
+      contentData: "送情书一直都很浪漫",
+      iconUrl:
+        "https://img.alicdn.com/imgextra/i4/O1CN01oQiXDF1mn8RoGCZyH_!!6000000004998-55-tps-20-20.svg",
       title: "写情书",
-    }
+    },
   ],
-  footerDesc: "服务生成的所有内容均由人工智能模型生成，其生成内容的准确性和完整性无法保证"
+  footerDesc:
+    "服务生成的所有内容均由人工智能模型生成，其生成内容的准确性和完整性无法保证",
 };
 
 export const darkTheme = {
@@ -74,7 +75,7 @@ export const darkTheme = {
   "--prompt-hover-bg": "transparent",
   "--prompt-hover-before-bg": "#F0F0F0",
   "--button-disabled-bg": "#dcdcdc",
-  "--border-hover-color": "#F0F0F0", 
+  "--border-hover-color": "#F0F0F0",
   "--border-shadow-color": "rgba(#bbb, .2)",
   "--send-btn-bg": "#434343",
   "--mobile-button-disabled-bg": "rgba(#888, 0.24)",
@@ -83,14 +84,15 @@ export const darkTheme = {
   "--mobile-question-bg": "#434343",
   "--mobile-question-color": "#fff",
   "--markdown-table-tr-border-color": "rgba(0, 0, 0, .08)",
-}
+};
 
 export const primaryTheme = {
   "--primary-color": "#615ced",
   "--answer-bg-color": "#615ced",
   "--primary-color-2": "#624aff",
   "--primary-button-bg": "linear-gradient(75deg, #615ced -8%, #3e2fa7 181%)",
-  "--primary-button-hover-bg": "linear-gradient(79deg, #746ff4 0%, #3820d9 181%)",
+  "--primary-button-hover-bg":
+    "linear-gradient(79deg, #746ff4 0%, #3820d9 181%)",
   "--primary-text-color": "#26244c",
   "--text-gray-1": "#3f3f3f",
   "--module-bg": "#f7f8fc",
@@ -108,28 +110,29 @@ export const primaryTheme = {
   "--gradient-bg-color": "#fff",
   "--prompt-hover-bg": "rgba(97, 92, 237, 0.06)",
   "--prompt-hover-before-bg": "#fff",
-  "--border-hover-color": "rgba(195, 197, 217, 0.65)", 
+  "--border-hover-color": "rgba(195, 197, 217, 0.65)",
   "--border-shadow-color": "rgba(115, 110, 240, 0.1)",
   "--button-disabled-bg": "#dcdcdc",
   "--send-btn-bg": "linear-gradient(47deg, #615ced 0%, #3e2fa7 176%)",
   "--mobile-button-disabled-bg": "rgba(135, 138, 171, 0.24)",
   "--mobile-button-disabled-color": "rgba(63, 63, 63, 0.5)",
-  "--mobile-button-focus-bg": "linear-gradient(68deg, #615ced 0%, #3e2fa7 180%)",
+  "--mobile-button-focus-bg":
+    "linear-gradient(68deg, #615ced 0%, #3e2fa7 180%)",
   "--mobile-question-bg": "linear-gradient(75deg, #615ced -3%, #3e2fa7 249%)",
   "--mobile-question-color": "#fff",
   "--markdown-table-tr-border-color": "rgba(0, 0, 0, .08)",
-}
+};
 
 export const changeTheme = (themeObj: Record<string, string>) => {
   const bodyStyle = document.body.style;
 
-  Object.keys(themeObj).map(itemKey => {
+  Object.keys(themeObj).map((itemKey) => {
     bodyStyle.setProperty(itemKey, themeObj[itemKey]);
   });
-}
+};
 
 export const changeMetaIcon = (iconUrl: string) => {
   const link = document.querySelector("link[rel='shortcut icon']");
   if (!link) return;
   link.href = iconUrl;
-}
+};
