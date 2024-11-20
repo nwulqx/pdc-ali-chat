b23cff271f0248ccf8f1ff495fbdcf9e188764f4
nwulqx <nwulqx@foxmail.com>
2024-11-20 15:16:24 +0800

    feat: 添加车机主页

--- newFrontend/src/hooks/useCanvasNest.ts
+++ newFrontend/src/hooks/useCanvasNest.ts
@@ -6,10 +6,9 @@ export const useCanvasNest = () => {
     const config = {
       color: '205,164,95', // 香槟金 #CDA45F
       pointColor: '218,165,32', // 金色 #DAA520
-      count: 188,
+      count: 120,
       opacity: 0.9,
       zIndex: 0,
-      lineWidth: 0.6,
       dist: 1000,
     };
 
