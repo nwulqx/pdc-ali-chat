c0a6ea4ee2a3bdb5a4128d2d00a5e91d1ea9d3b1
zhenguow <zhenguow@wicresoft.com>
2024-11-22 18:48:31 +0800

    添加聊天记忆

--- backend/src/main/java/com/aliyun/bailian/chatgpt/config/WebConfig.java
+++ backend/src/main/java/com/aliyun/bailian/chatgpt/config/WebConfig.java
@@ -1,7 +1,12 @@
 package com.aliyun.bailian.chatgpt.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.text.SimpleDateFormat;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/audio/**")
                 .addResourceLocations("file:" + uploadAudioDir + "/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(jackson2HttpMessageConverter);
     }
 }
--- backend/src/main/java/com/aliyun/bailian/chatgpt/controller/LlmDashSseController.java
+++ backend/src/main/java/com/aliyun/bailian/chatgpt/controller/LlmDashSseController.java
@@ -1,7 +1,25 @@
 package com.aliyun.bailian.chatgpt.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.aliyun.bailian.chatgpt.client.LLmDashClient;
+import com.aliyun.bailian.chatgpt.config.LlmConfig;
+import com.aliyun.bailian.chatgpt.constants.Constants;
 import com.aliyun.bailian.chatgpt.dto.DashLlmRequestDTO;
+import com.aliyun.bailian.chatgpt.dto.MessageDto;
+import com.aliyun.bailian.chatgpt.model.PosAutoLog;
+import com.aliyun.bailian.chatgpt.service.PosAutoLogService;
+import com.aliyun.bailian.chatgpt.service.StringRedisService;
+import com.aliyun.bailian.chatgpt.utils.R;
+import com.aliyun.broadscope.bailian.sdk.ApplicationClient;
+import com.aliyun.broadscope.bailian.sdk.models.CompletionsRequest;
+import com.aliyun.broadscope.bailian.sdk.models.CompletionsResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -11,6 +29,51 @@ public class LlmDashSseController {
 
   @Autowired
   private LLmDashClient llmDashClient;
+
+  @Autowired
+  private StringRedisService stringRedisService;
+
+  @javax.annotation.Resource
+  private LlmConfig llmConfig;
+
+  @Autowired
+  private PosAutoLogService posAutoLogService;
+
+  @Autowired
+  private ApplicationClient applicationClient;
+
+  List<String> list = List.of("1.开关窗户", "2.开关音乐", "3.开关电视机");
+
+
+  @PostMapping(value = "/checkMessage")
+  public R<Object> checkMessage(@RequestBody MessageDto message) throws IOException {
+    String msg = stringRedisService.getKey(Constants.YX_PROMPT);
+//    if (StringUtils.isBlank(msg)) {
+//      msg = new String(Files.readAllBytes(Paths.get(resource.getURI())));
+//    }
+    String ability = stringRedisService.getKey(Constants.ABILITY);
+    if (StringUtils.isNotBlank(ability)) {
+      msg = msg.replace("{list}", ability);
+    } else {
+      msg = msg.replace("{list}", JSON.toJSONString(list));
+    }
+    msg = msg.replace("{message}", message.getMessage());
+    msg = msg + ",用户输入:" + message;
+    CompletionsRequest completionsRequest = new CompletionsRequest();
+    completionsRequest.setPrompt(msg);
+    completionsRequest.setAppId(llmConfig.getAppIds().getOrDefault("text_chat", null));
+    CompletionsResponse completions = applicationClient.completions(completionsRequest);
+    String text = completions.getData().getText();
+    JSONObject jsonObject = JSON.parseObject(text);
+    PosAutoLog log = new PosAutoLog();
+    log.setUserId(StringUtils.defaultString(message.getSessionId(), "system"));
+    log.setQuestion(message.getMessage());
+    log.setAnswer(jsonObject.toJSONString());
+    log.setCreateTime(LocalDateTime.now());
+    posAutoLogService.savePosAutoLog(log);
+    return R.success(jsonObject);
+  }
+
 
   // 提供文本流 SSE 服务
   @PostMapping("/v1/stream-text")
